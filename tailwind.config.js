b26cb07cab83b22cc51c28422f24ed0727c70785
Lee Sheppard <lee@leesheppard.com>
2023-10-06 14:23:21 +1100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,5 +4,13 @@ module.exports = {
     './app/helpers/**/*.rb',
     './app/assets/stylesheets/**/*.css',
     './app/javascript/**/*.js'
-  ]
+  ],
+  theme: {
+    container: {
+      center: true,
+    },
+  },
+  corePlugins: {
+    preflight: false,
+  }
 }
